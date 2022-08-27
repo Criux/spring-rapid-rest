64277c7cd71f3d12ee70bf55be2a89bcd8572252
Criux <greekmule@gmail.com>
2022-08-27 09:17:28 +0200

    initial commit

---
+++ src/main/java/com/kmarinos/springrapidrest/SpringRapidRestApplication.java
@@ -0,0 +1,13 @@
+package com.kmarinos.springrapidrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRapidRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRapidRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmarinos/springrapidrest/SpringRapidRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmarinos.springrapidrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRapidRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
